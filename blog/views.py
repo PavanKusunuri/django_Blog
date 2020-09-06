e5705bf4465a0e28f7cb4df5a2d44d18fa9b298a
Pavan <pavantejakusunuri@gmail.com>
2020-09-06 09:48:08 +0530

    Updated Delete Url and method so we can delete post

--- blog/views.py
+++ blog/views.py
@@ -25,3 +25,9 @@ class BlogUpdateView(UpdateView):
     model = Post
     template_name = 'post_edit.html'
     fields = ['title', 'body']
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
